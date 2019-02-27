514e7867c4b5abad6392e35efd1d5fce57b45d1b
RyKizil <eray_kizilmese@hotmail.com>
2019-02-27 11:22:37 +0100

    added signup component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+
 
 @NgModule({
-    imports: [],
-    exports: []
+    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule]
 })
 export class MaterialModule{
 
