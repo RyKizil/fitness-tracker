88c36fb8326c6bc21b55e25071c3f83c82579f66
RyKizil <eray_kizilmese@hotmail.com>
2019-03-02 09:57:40 +0100

    add temp trainings-component and timer functions

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { StopTrainingComponent} from './stop-training.component';
+
 
 @Component({
   selector: 'app-current-training',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+@Output() exitOrNot = new EventEmitter();
+progress: number = 0;
+timer: number;
+message: string = 'stop';
 
-  constructor() { }
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startTimer();
+  }
+  startTimer(){
+    this.timer = setInterval(() => {
+      this.progress += 5;
+      if(this.progress >= 100){
+        clearInterval(this.timer);
+      }
+    }, 500);
+  }
+
+
+  stopTimer(){
+      clearInterval(this.timer);
+      const dialogRef =  this.dialog.open(StopTrainingComponent, {
+        data: {
+          progress: this.progress
+        }
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if(result){
+          this.exitOrNot.emit();
+        }else{
+          this.startTimer();
+        }
+
+      })
+    
   }
 
 }
